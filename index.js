b40918de676adfea3e9b73b2ca2a72dc18d778c6
Aaron Storck <aaronstorck@gmail.com>
2015-06-13 19:01:17 -0400

    Refactored SuperJS to Ouro.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ YSS'      YSSP~YSSY    S*S           YSSP  S*S    SSS  YSSY    YSS'
                        SP                  SP
                        Y                   Y
 
-            -= SuperJS Express Server Module Loader =-
+            -= Ouro Express Server Module Loader =-
 
 */
 
